078b36901b24cedc3b1fc973cbba28b22f423f77
dmheywood <davidmheywood@gmail.com>
2015-11-10 11:32:54 -0500

    Adding comparison counting to a comparator
    The basic idea is:
    add a counter inside the comparator.
    declare that the comparator class implements cmpCnt
    the constructor initializes this counter to zero.
    
    after we are done sorting or searching, we cast the comparator to cmpCnt
    and call getCmpCnt to retrieve the comparison count from inside the
    comparator

---
+++ CmpCnt.java
@@ -0,0 +1,29 @@
+/**
+ * CmpCnt.java
+ * Bob Boothe Oct 2007
+ * 
+ * Interface for adding comparison counting to a comparator	in a clean fashion.
+ * 	
+ * You must add a counter and implement getCmpCnt() & resetCmpCnt()
+ * 
+ * The basic idea is:
+ *    we add a counter inside the comparator.
+ *    we declare that the comparator class implements cmpCnt
+ *    the constructor initializes this counter to zero
+ *    
+ *    after we are done sorting or searching, we cast the comparator to cmpCnt
+ *    and call getCmpCnt to retrieve the comparison count from inside the comparator
+ */
+public interface CmpCnt {
+
+    /**
+     *
+     * @return the value of the embedded counter
+     */
+    int getCmpCnt();
+
+    /**
+     * reset the counter to 0
+     */
+    void resetCmpCnt();
+}
